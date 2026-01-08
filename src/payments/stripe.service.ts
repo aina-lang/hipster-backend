c6624e43c6036f594261040beb5d50cf0bb6085c
aina-lang <merciaaina@gmail.com>
2026-01-08 22:23:10 +0300

    Stripe configuration

--- src/payments/stripe.service.ts
+++ src/payments/stripe.service.ts
@@ -7,11 +7,7 @@ export class StripeService {
   private stripe: Stripe;
 
   constructor(private readonly configService: ConfigService) {
-<<<<<<< HEAD
-    const apiKey = this.configService.get<string>('STRIPE_SECRET_KEY');
-=======
-     const apiKey = this.configService.get<string>('STRIPE_SECRET');
->>>>>>> 1a967fb (Clean squashed commit)
+    const apiKey = this.configService.get<string>('STRIPE_SECRET');
     if (apiKey) {
       this.stripe = new Stripe(apiKey, {
         apiVersion: '2025-11-17.clover',
