62b82eca8a5def4f72fb15e84d4d267326133c96
aina-lang <merciaaina@gmail.com>
2026-01-08 22:16:54 +0300

    Clean squashed commit

--- src/payments/stripe.service.ts
+++ src/payments/stripe.service.ts
@@ -7,7 +7,11 @@ export class StripeService {
   private stripe: Stripe;
 
   constructor(private readonly configService: ConfigService) {
+<<<<<<< HEAD
     const apiKey = this.configService.get<string>('STRIPE_SECRET_KEY');
+=======
+     const apiKey = this.configService.get<string>('STRIPE_SECRET');
+>>>>>>> 1a967fb (Clean squashed commit)
     if (apiKey) {
       this.stripe = new Stripe(apiKey, {
         apiVersion: '2025-11-17.clover',
