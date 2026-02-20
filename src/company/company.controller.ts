4597430c63fb7f173ac67593e045b0cf8e613009
aina-lang <merciaaina@gmail.com>
2026-02-20 16:52:17 +0300

    feat: allow file upload destination to be configured via `UPLOAD_PATH` environment variable.

--- src/company/company.controller.ts
+++ src/company/company.controller.ts
@@ -55,7 +55,9 @@ export class CompanyController {
   @UseInterceptors(
     FileInterceptor('file', {
       storage: diskStorage({
-        destination: join(process.cwd(), 'uploads'),
+        destination: (req, file, cb) => {
+          cb(null, process.env.UPLOAD_PATH || join(process.cwd(), 'uploads'));
+        },
         filename: (req, file, cb) => {
           const randomName = Array(32)
             .fill(null)
